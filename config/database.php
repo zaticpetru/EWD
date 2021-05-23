fd7b14cdb95e74fc57b845f2a173da5729d7e48f
zaticpetru <zatic.petru@outlook.com>
2021-05-24 00:17:53 +0300

    database connection

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+    class Database {
+        private $host = "";
+        private $db = "";
+        private $username  = "root";
+        private $password = "";
+        private $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        public $connection;
+
+        public function getConnection() {
+            $this->connection = null;
+            try {
+                $this->connection = new PDO("mysql:host=$this->host;dbname=$this->db",
+                                        $this->username,
+                                        $this->password,
+                                        $this->options);
+                $this->connection->exec("set names utf8");
+            } catch(PDOException $exception) {
+                echo "Database not connected: " . $exception->getMessage();
+            }
+
+            return $this->connection;
+        }
+    }
+?>
